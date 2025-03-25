38974609e2443e3f379e2be71fcbfe83796e2f24
Koakovskie <hkoakovskip@gmail.com>
2025-03-25 17:16:41 +0000

    refactor: refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-function App() {
+import { useEffect, useState } from "react";
+import { searchCardsService } from "./services/scryfall-api/services/cards/serch-cards.service";
+import { Card } from "./services/scryfall-api/types/card";
+
+const App = () => {
+  const [cards, setCards] = useState<Card[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await searchCardsService({ text: "sol" });
+      if (result.success) {
+        setCards(result.data.data);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <div>Initial</div>
-    </>
+    <div>
+      {cards.length ? (
+        <ul>
+          {cards.map((card) => (
+            <li key={card.id}>{card.name}</li>
+          ))}
+        </ul>
+      ) : (
+        "Carregando..."
+      )}
+    </div>
   );
-}
+};
 
 export default App;
