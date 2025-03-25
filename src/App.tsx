f2f51461fe419c62d1bf6c16a33df71ea2550dfe
Koakovskie <hkoakovskip@gmail.com>
2025-03-25 18:00:52 -0300

    feat: apply logic to open SearchCardModal

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,35 @@ import GridItem from "./components/GridItem";
 import CardItem from "./components/Card";
 import { CardEntity } from "./domain/entities/card.entity";
 import AddCardButton from "./components/AddCardButton";
+import SearchCardModal from "./components/SearchCardModal";
 
 const App = () => {
   const [cards, setCards] = useState<CardEntity[]>([]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <div>
-      <Grid>
-        <GridItem key={"add_card_button"}>
-          <AddCardButton />
-        </GridItem>
-        {cards.map((card) => (
-          <GridItem key={card.id}>
-            <CardItem card={card} />
+    <>
+      {isOpen && (
+        <SearchCardModal
+          close={() => {
+            setIsOpen(false);
+          }}
+        />
+      )}
+
+      <div className="mr-30 ml-30">
+        <Grid>
+          <GridItem key={"add_card_button"}>
+            <AddCardButton onClick={() => setIsOpen(true)} />
           </GridItem>
-        ))}
-      </Grid>
-    </div>
+          {cards.map((card) => (
+            <GridItem key={card.id}>
+              <CardItem card={card} />
+            </GridItem>
+          ))}
+        </Grid>
+      </div>
+    </>
   );
 };
 
--- src/components/AddCardButton/index.tsx
+++ src/components/AddCardButton/index.tsx
@@ -1,8 +1,13 @@
 import { FC } from "react";
 
-const AddCardButton: FC = () => {
+type AddCardButtonProps = {
+  onClick: () => void;
+};
+
+const AddCardButton: FC<AddCardButtonProps> = ({ onClick }) => {
   return (
     <button
+      onClick={onClick}
       className={`w-full h-full bg-blue-500 text-white rounded-lg hover:bg-blue-600 cursor-pointer`}
     >
       +
