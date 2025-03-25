f7bc34eb08b417b4a9f044b3dfbb89ec88cf2a36
Koakovskie <hkoakovskip@gmail.com>
2025-03-25 18:00:40 -0300

    feat: added SearchCardModal

---
+++ src/components/SearchCardModal/index.tsx
@@ -0,0 +1,78 @@
+import { FC, useEffect, useState } from "react";
+import { CardEntity } from "../../domain/entities/card.entity";
+import { searchCardsService } from "../../services/scryfall-api/services/cards/serch-cards.service";
+import Grid from "../Grid";
+import GridItem from "../GridItem";
+import CardItem from "../Card";
+
+type SearchCardModalProps = {
+  close: () => void;
+};
+
+const SearchCardModal: FC<SearchCardModalProps> = ({ close }) => {
+  const [query, setQuery] = useState("");
+  const [typingTimeout, setTypingTimeout] = useState<number | null>(null);
+
+  const [cards, setCards] = useState<CardEntity[]>([]);
+
+  const fetchData = async (text: string) => {
+    const result = await searchCardsService({ text });
+    if (result.success) {
+      setCards(result.data.data.map((card) => CardEntity.new(card)));
+    }
+  };
+
+  useEffect(() => {
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
+    }
+
+    const timeout = setTimeout(() => {
+      if (query.trim()) {
+        fetchData(query.trim());
+      } else {
+        setCards([]);
+      }
+    }, 300);
+
+    setTypingTimeout(timeout);
+
+    return () => clearTimeout(timeout);
+  }, [query]);
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center">
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative w-9/10 h-9/10 bg-white p-6 rounded-lg flex flex-col">
+        <div className="flex-1">
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Pesquisar..."
+            className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none"
+          />
+
+          <Grid>
+            {cards.map((card) => (
+              <GridItem key={card.id}>
+                <CardItem card={card} />
+              </GridItem>
+            ))}
+          </Grid>
+        </div>
+
+        <div className="mt-4 flex justify-end">
+          <button
+            onClick={close}
+            className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
+          >
+            Fechar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchCardModal;
