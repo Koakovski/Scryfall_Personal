8d1e00887862959666bd9cf47387351cebf53488
Koakovskie <hkoakovskip@gmail.com>
2025-03-25 19:20:21 +0000

    remove logic to fetch cards on APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Grid from "./components/Grid";
 import GridItem from "./components/GridItem";
 import CardItem from "./components/Card";
 import { CardEntity } from "./domain/entities/card.entity";
-import { searchCardsService } from "./services/scryfall-api/services/cards/serch-cards.service";
 import AddCardButton from "./components/AddCardButton";
 
 const App = () => {
   const [cards, setCards] = useState<CardEntity[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await searchCardsService({ text: "sol" });
-      if (result.success) {
-        setCards(result.data.data.map((card) => CardEntity.new(card)));
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <div>
